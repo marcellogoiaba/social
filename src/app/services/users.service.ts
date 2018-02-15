97a9f43eb33c71b18009d0c1a185ab5930c7f6bf
Marcelo <marcellogoiaba@gmail.com>
2018-02-15 17:30:09 +0000

    started with user service and model

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'xrjs/add/operator/map';
+import  Utils  from '../utils/utils'
+
+@Injectable()
+export class UsersService {
+
+  constructor() { }
+
+}
